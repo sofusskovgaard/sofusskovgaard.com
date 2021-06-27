bc63ce5071831f16639000d0202fcb86555e9971
Sofus Skovgaard <sofus.skovgaard@gmail.com>
2021-06-27 10:03:10 +0200

    Migrated to using flex instead of grid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,12 +11,18 @@ import Repository from 'components/repository'
 import List from 'components/list'
 import Container from 'components/container'
 import Loader from 'components/loader'
+import styled from 'styled-components'
 
 const Home: NextPage = () => {
   return (
-    <Container className="grid gap-4 grid-cols-1 md:grid-cols-2 pt-4 md:pt-12 pb-4">
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 grid-rows-3 md:grid-rows-6 md:col-span-2">
-        <div className="col-start-1 row-start-2 md:row-start-1 md:row-span-6">
+    <Container className="pt-4 md:pt-12 pb-4">
+      <div className="flex flex-col md:flex-row">
+        <section className="block md:hidden mb-4">
+          <h1 className="font-bold text-3xl mb-1">Sofus Skovgaard</h1>
+          <h3 className="font-medium uppercase text-gray-500">Developer / Designer</h3>
+        </section>
+
+        <div className="flex-1 mb-4 md:mb-0">
           <Image
             src="/images/sofus_skovgaard.jpg"
             className="rounded"
@@ -27,22 +33,24 @@ const Home: NextPage = () => {
           />
         </div>
 
-        <section className="row-start-1 col-start-1 md:col-start-2">
-          <h1 className="font-bold text-3xl mb-1">Sofus Skovgaard</h1>
-          <h3 className="font-medium uppercase text-gray-500">Developer / Designer / Photographer</h3>
-        </section>
+        <div className="flex flex-col flex-1 md:ml-4">
+          <section className="hidden md:block mb-4">
+            <h1 className="font-bold text-3xl mb-1">Sofus Skovgaard</h1>
+            <h3 className="font-medium uppercase text-gray-500">Developer / Designer</h3>
+          </section>
 
-        <section className="row-start-3 md:col-start-2 md:row-span-5">
-          <p className="mb-4">
-            Culpa nostrud sit et ex aute ea. Deserunt deserunt ad cillum deserunt veniam duis ipsum eu elit sit. Id ea
-            qui velit nostrud ullamco.
-          </p>
-          <p>
-            Quis non est reprehenderit fugiat tempor aliqua. Lorem deserunt enim enim aliqua laborum dolore ullamco
-            voluptate culpa adipisicing aliqua laboris Lorem. Ad qui est ea nostrud irure duis adipisicing et minim
-            Lorem eiusmod ipsum consectetur.
-          </p>
-        </section>
+          <section>
+            <p className="mb-4">
+              Culpa nostrud sit et ex aute ea. Deserunt deserunt ad cillum deserunt veniam duis ipsum eu elit sit. Id ea
+              qui velit nostrud ullamco.
+            </p>
+            <p>
+              Quis non est reprehenderit fugiat tempor aliqua. Lorem deserunt enim enim aliqua laborum dolore ullamco
+              voluptate culpa adipisicing aliqua laboris Lorem. Ad qui est ea nostrud irure duis adipisicing et minim
+              Lorem eiusmod ipsum consectetur.
+            </p>
+          </section>
+        </div>
       </div>
 
       {/* <List<PostModel>
@@ -51,9 +59,9 @@ const Home: NextPage = () => {
         link={{ href: '#', text: 'All posts' }}
         render={(model) => <Post key={model.id} post={model} />}
         model={posts}
-      /> */}
+      />
 
-      {/* <List<RepositoryModel>
+      <List<RepositoryModel>
           className="md:col-start-1 md:row-start-2"
           title="Repositories"
           link={{ href: '#', text: 'All repositories' }}
