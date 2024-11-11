fdd05270c2acf7904c8c8ce52bf3b16ba11f3bf3
Sofus Skovgaard <sofus@skovgaard.io>
2024-11-11 21:50:04 +0100

    chore: updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,27 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  mode: "jit",
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      sans: ['-apple-system', 'Inter', '"Segoe UI"'],
-      mono: ['ui-monospace', 'Roboto Mono', 'monospace'],
+      sans: ["-apple-system", "Inter", '"Segoe UI"'],
+      mono: ["ui-monospace", "Roboto Mono", "monospace"],
+    },
+    screens: {
+      "xxs": "384px",
+      "xs": "512px",
+      "sm": "640px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px",
     },
   },
   variants: {
     extend: {
-      borderStyle: ['hover'],
+      borderStyle: ["hover"],
     },
   },
-  plugins: [require('@tailwindcss/typography')],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
